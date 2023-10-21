3cf60f224d282a3351711016edcbfc76ed468105
TanKhalid <143603131+TanKhalid@users.noreply.github.com>
2023-10-21 22:31:48 +0300

    JS_4_Exercise_2

--- mainscript.js
+++ mainscript.js
@@ -1,5 +1,5 @@
 
-window.addEventListener('load', () => {
+window.addEventListener('load', e => {
   const preload = document.querySelector('.preloader')
   setTimeout( function() {
       preload.classList.add('preloader-finish')
@@ -16,27 +16,27 @@ showSlides(slideIndex);
 
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides(slideIndex += n)
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides(slideIndex = n)
 }
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  let slides = document.getElementsByClassName("mySlides")
+  let dots = document.getElementsByClassName("dot")
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none"  
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "")
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex-1].style.display = "block"  
+  dots[slideIndex-1].className += " active"
 
 
 
